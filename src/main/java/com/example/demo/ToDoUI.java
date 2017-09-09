9264d58fbe097fc3f5f0b18a240b74ddc26f9fa8
chandimaw <chandimaw@mllenniumit.com>
2017-09-09 19:31:32 +0530

    update with css

--- src/main/java/com/example/demo/ToDoUI.java
+++ src/main/java/com/example/demo/ToDoUI.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
 
+import com.vaadin.icons.VaadinIcons;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.*;
+import com.vaadin.ui.themes.ValoTheme;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringUI
@@ -24,21 +26,31 @@ public class ToDoUI extends UI {
 
     private void setLayout() {
         root = new VerticalLayout();
+        root.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
         setContent(root);
     }
 
     private void addHeader() {
-        root.addComponent(new Label("TODOs"));
+
+        Label labelHeader = new Label("TODOs");
+        labelHeader.addStyleName(ValoTheme.LABEL_H1);
+        root.addComponent(labelHeader);
+
     }
     private void addList() {
+        todolayot.setWidth("80%");
         root.addComponent(todolayot);
     }
 
     private void addForm() {
         HorizontalLayout horizontalLay = new HorizontalLayout();
+        horizontalLay.setWidth("80%");
         TextField task = new TextField();
-        Button add = new Button( "Add");
-        horizontalLay.addComponents(task,add);
+        Button add = new Button();
+        add.setStyleName(ValoTheme.BUTTON_PRIMARY);
+        add.setIcon(VaadinIcons.PLUS);
+        horizontalLay.addComponentsAndExpand(task);
+        horizontalLay.addComponents(add);
         root.addComponent(horizontalLay);
     }
 
